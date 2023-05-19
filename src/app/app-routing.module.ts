561eb746c8b4f09eacbdefe68a7ca645629f9ab9
reda <redabusiness10@gmail.com>
2023-05-19 23:53:26 +0000

    First

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthentificationComponent } from './components/authentification/authentification.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'auth', component: AuthentificationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
